2d98fb3455b29ca696a71c246cb5330a5de45252
carlosJimenez2 <42255224+carlosJimenez2@users.noreply.github.com>
2018-08-16 11:49:38 -0500

    Add files via upload

---
+++ Automovil.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transporte
+{
+    class Automovil
+    {
+        public string Color;
+        public string Marca;
+        public int Modelo;
+        enum AIREACONDICIONADO { APAGADO, FRIO, CALIENTE}
+        enum INTENSIDADDELUCES { BAJAS, NORMALES, ALTAS }
+    
+        public Automovil()
+        {
+            Marca = "BMW";
+            Modelo = 2018;
+            Color = "Negro";
+        }
+
+        public Automovil(string marca, int modelo, string color)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Color = color;
+        }
+
+        public bool Luces(int intensidad)
+        {
+            bool resultado = false;
+            if (intensidad == (int)INTENSIDADDELUCES.ALTAS)
+            {
+                resultado = false;
+            }
+            else if (intensidad == (int)INTENSIDADDELUCES.BAJAS)
+            {
+                resultado = true;
+            }
+            else if (intensidad == (int)INTENSIDADDELUCES.NORMALES)
+            {
+                resultado = true;
+            }
+
+            return resultado;
+        }
+        public bool Aire (int funcionamiento)
+        {
+            bool respuesta = false;
+            if (funcionamiento == (int)AIREACONDICIONADO.APAGADO)
+            {
+                respuesta = false;
+            }
+            else if (funcionamiento == (int)AIREACONDICIONADO.FRIO)
+            {
+                respuesta = true;
+            }
+            else if (funcionamiento == (int)AIREACONDICIONADO.CALIENTE)
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Transporte
+{
+    class Program
+    {
+        enum AIREACONDICIONADO { APAGADO, FRIO, CALIENTE }
+        enum INTENSIDADDELUCES {BAJAS, NORMALES, ALTAS}
+        static void Main(string[] args)
+        {
+            bool intensidades = false;
+
+            Automovil Automovil1 = new Automovil();
+
+            Automovil1.Marca = "MERCEDES BENZ";
+            Automovil1.Modelo = 2017;
+            Automovil1.Color = "Blanco";
+
+            intensidades = Automovil1.Luces((int)INTENSIDADDELUCES.BAJAS);
+
+            if (intensidades)
+            {
+                Console.WriteLine("El automovil " + Automovil1.Marca + " tiene las luces " + Convert.ToString(INTENSIDADDELUCES.ALTAS) + " encendidas");
+            }
+            else
+            {
+                Console.WriteLine("Aun no apagan las luces");
+            }
+
+            Automovil Automovil2 = new Automovil();
+            Console.WriteLine("El automovil " + Automovil2.Marca + " tiene las luces " + Convert.ToString(INTENSIDADDELUCES.BAJAS) + " encendidas");
+            
+
+            bool temperatura = false;
+            temperatura = Automovil1.Aire((int)AIREACONDICIONADO.APAGADO);
+            
+            if (temperatura)
+            {
+                Console.WriteLine("El automovil " + Automovil1.Marca + " tiene el aire acondionado  " + Convert.ToString(AIREACONDICIONADO.FRIO) + " encendido");
+            }
+            else
+            {
+                Console.WriteLine("Checar el funcionamiento del aire del automovil " + Automovil1.Marca);
+            }
+            Automovil Automovil3 = new Automovil();
+            Console.WriteLine("El automovil " + Automovil3.Marca + " tiene el aire acondiconado " + Convert.ToString(AIREACONDICIONADO.CALIENTE) + " encendido");
+            Console.Read();
+        }   
+
+    }
+}
